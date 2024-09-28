f867ee6fe4296c68ebbb8e791ab044c1bd3b8e6b
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-09-28 23:41:08 +0200

    added pcloud

---
+++ Casks/pcloud.rb
@@ -0,0 +1,15 @@
+cask "pcloud" do
+  version "3.15.2"
+  code "XZJPkU0Zsz7DuTM5Bb7YhYyHJJJmLQhdVorX"
+  sha256 "199bd79a1c93b00e0de4569159fb26d0c616190ce5a8c92d268300ffaf040fed"
+
+  url "https://api.pcloud.com/getpubzip?code=#{code}&filename=pcloud-#{version}.zip",
+  name "pCloud"
+  desc "pCloud"
+  homepage "https://www.pcloud.com"
+
+  depends_on macos: ">= :big_sur"
+
+  pkg "pCloud Drive #{version} UNIVERSAL.pkg"
+  uninstall pkgutil: "com.pcloud.pcloud.macos"
+end
