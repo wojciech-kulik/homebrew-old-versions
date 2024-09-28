2787c86f0d93f05414d79b217b052279ddd363a2
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-09-28 23:07:33 +0200

    added sketch + bartender

---
+++ Casks/bartender-4.2.25.rb
@@ -0,0 +1,29 @@
+cask "bartender" do
+  version "4.2.25"
+  sha256 "7e594d4955808df139bc3aa4a408a8bacbfdb64e18970e843d381eadce487299"
+
+  url "https://macbartender.com/B2/updates/#{version.major}-#{version.minor}-#{version.patch.rjust(1, "0")}/Bartender%20#{version.major}.dmg",
+      referer: "https://www.macbartender.com"
+  name "Bartender"
+  desc "Menu bar icon organizer"
+  homepage "https://www.macbartender.com/"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Bartender #{version.major}.app"
+
+  uninstall delete:    [
+              "/Library/Audio/Plug-Ins/HAL/BartenderAudioPlugIn.plugin",
+              "/Library/PrivilegedHelperTools/com.surteesstudios.Bartender.BartenderInstallHelper",
+              "/Library/ScriptingAdditions/BartenderHelper.osax",
+              "/System/Library/ScriptingAdditions/BartenderSystemHelper.osax",
+            ],
+            launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
+            quit:      "com.surteesstudios.Bartender"
+
+  zap trash: [
+    "~/Library/Caches/com.surteesstudios.Bartender",
+    "~/Library/Cookies/com.surteesstudios.Bartender.binarycookies",
+    "~/Library/Preferences/com.surteesstudios.Bartender.plist",
+  ]
+end
---
+++ Casks/sketch-76.1.rb
@@ -0,0 +1,27 @@
+cask "sketch" do
+  version "76.1,130273"
+  sha256 "3e86a9be50b84719a1fad0964febed4af5122a4f143b2dd9c4ddd166bcc1d977"
+
+  url "https://download.sketchapp.com/sketch-#{version.before_comma}-#{version.after_comma}.zip"
+  name "Sketch"
+  desc "Digital design and prototyping platform"
+  homepage "https://www.sketchapp.com/"
+
+  depends_on macos: ">= :mojave"
+
+  app "Sketch.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*",
+    "~/Library/Application Support/com.bohemiancoding.sketch3",
+    "~/Library/Caches/com.bohemiancoding.sketch3",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3",
+    "~/Library/Logs/com.bohemiancoding.sketch3",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.plist",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/Saved Application State/com.bohemiancoding.sketch3.savedState",
+    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
+  ]
+end
