e01b86d35878e8e7c1d033ae39f8c369e635c608
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-12-07 13:20:11 +0100

    Update pcloud

--- Casks/pcloud.rb
+++ Casks/pcloud.rb
@@ -1,6 +1,6 @@
 cask "pcloud" do
   version "4.0.5,d3gz7dDhEVHefkyYOk51k0"
-  sha256 "8f89b76e537780dc8b145eeebcbbb0fd76aadf9673daea41f6cf5389bfe89fee"
+  sha256 "bea95927b9fbce1a369bb56cd8c99cf1911d1ef8543ed388010f3872b18aae9f"
 
   url "https://api.pcloud.com/getpubzip?code=#{version.after_comma}&filename=pcloud-#{version.before_comma}.zip"
   name "pCloud"
